29928d4f40b29d2c59dfe00dcea233e5144914e8
Mojtaba Nematpour <mjtba.nmtpr@gmail.com>
2020-12-12 18:47:10 +0330

    Send payment request

--- controllers/front/validation.php
+++ controllers/front/validation.php
@@ -1,28 +1,29 @@
 <?php
+
 /**
-* 2007-2020 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author    PrestaShop SA <contact@prestashop.com>
-*  @copyright 2007-2020 PrestaShop SA
-*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*/
+ * 2007-2020 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2020 PrestaShop SA
+ * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  International Registered Trademark & Property of PrestaShop SA
+ */
 class Ps_pgzarinpalValidationModuleFrontController extends ModuleFrontController
 {
     /**
@@ -35,50 +36,33 @@ class Ps_pgzarinpalValidationModuleFrontController extends ModuleFrontController
          * If the module is not active anymore, no need to process anything.
          */
         if ($this->module->active == false) {
-            die;
+            // Redirect the customer to cart controller
+            Tools::redirect($this->context->link->getPageLink('cart?action=show'));
         }
 
-        /**
-         * Since it is an example, we choose sample data,
-         * You'll have to get the correct values :)
-         */
-        $cart_id = 1;
-        $customer_id = 1;
-        $amount = 100.00;
+        //Get cart info by Context
+        $cart = Context::getContext()->cart;
+        $customer = Context::getContext()->customer;
 
-        /*
-         * Restore the context from the $cart_id & the $customer_id to process the validation properly.
-         */
-        Context::getContext()->cart = new Cart((int) $cart_id);
-        Context::getContext()->customer = new Customer((int) $customer_id);
-        Context::getContext()->currency = new Currency((int) Context::getContext()->cart->id_currency);
-        Context::getContext()->language = new Language((int) Context::getContext()->customer->id_lang);
+        $amount = $cart->getOrderTotal();
+        $description = $customer->firstname . " " . $customer->lastname;
+        $email = $customer->email;
+        $mobile = (new Address($cart->id_address_invoice))->phone;
 
-        $secure_key = Context::getContext()->customer->secure_key;
-
-        if ($this->isValidOrder() === true) {
-            $payment_status = Configuration::get('PS_OS_PAYMENT');
-            $message = null;
-        } else {
-            $payment_status = Configuration::get('PS_OS_ERROR');
-
-            /**
-             * Add a message to explain why the order has not been validated
-             */
-            $message = $this->module->l('An error occurred while processing payment');
+        $zarinpal = new ZarinPalGateway();
+        try {
+            $zarinpal->paymentRequest($amount, $description, $email, $mobile);
+        } catch (PrestaShopDatabaseException $e) {
+            echo $e->getMessage();
+        } catch (PrestaShopException $e) {
+            echo $e->getMessage();
         }
 
-        $module_name = $this->module->displayName;
-        $currency_id = (int) Context::getContext()->currency->id;
+        if ( $zarinpal->getPaymentError() ) {
+            echo $zarinpal->getPaymentError();
+            return $this->setTemplate('module:ps_pgzarinpal/views/templates/front/error.tpl', ['errors' => [$zarinpal->getPaymentError()]]);
+        }
 
-        return $this->module->validateOrder($cart_id, $payment_status, $amount, $module_name, $message, array(), $currency_id, false, $secure_key);
-    }
-
-    protected function isValidOrder()
-    {
-        /*
-         * Add your checks right there
-         */
         return true;
     }
 }
