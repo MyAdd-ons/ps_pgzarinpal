3eb58bb0005bc3e09e769ca06e260114c9b7df1a
Mojtaba Nematpour <mjtba.nmtpr@gmail.com>
2020-12-12 18:47:30 +0330

    Send verification request

--- controllers/front/confirmation.php
+++ controllers/front/confirmation.php
@@ -1,75 +1,101 @@
 <?php
+
 /**
-* 2007-2020 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author    PrestaShop SA <contact@prestashop.com>
-*  @copyright 2007-2020 PrestaShop SA
-*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*/
+ * 2007-2020 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2020 PrestaShop SA
+ * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  International Registered Trademark & Property of PrestaShop SA
+ */
 class Ps_pgzarinpalConfirmationModuleFrontController extends ModuleFrontController
 {
     public function postProcess()
     {
-        if ((Tools::isSubmit('cart_id') == false) || (Tools::isSubmit('secure_key') == false)) {
-            return false;
-        }
+        $status = Tools::getValue("Status");
+        $error = "";
 
-        $cart_id = Tools::getValue('cart_id');
-        $secure_key = Tools::getValue('secure_key');
-
-        $cart = new Cart((int) $cart_id);
-        $customer = new Customer((int) $cart->id_customer);
-
-        /**
-         * Since it's an example we are validating the order right here,
-         * You should not do it this way in your own module.
+        /*
+         * Check if cart purchased successfully
          */
-        $payment_status = Configuration::get('PS_OS_PAYMENT'); // Default value for a payment that succeed.
-        $message = null; // You can add a comment directly into the order so the merchant will see it in the BO.
+        if ($status == "OK") {
+            $authority = Tools::getValue("Authority");
 
-        /**
-         * Converting cart into a valid order
-         */
-        $module_name = $this->module->displayName;
-        $currency_id = (int) Context::getContext()->currency->id;
+            //Get cart id by authority
+            $id_pgzarinpal = PgzarinpalModel::getIdByAuthority($authority);
+            try {
+                $pgzarinpal = new PgzarinpalModel($id_pgzarinpal);
+            } catch (PrestaShopDatabaseException $e) {
+                $error = $e->getMessage();
+            } catch (PrestaShopException $e) {
+                $error = $e->getMessage();
+            }
 
-        $this->module->validateOrder($cart_id, $payment_status, $cart->getOrderTotal(), $module_name, $message, array(), $currency_id, false, $secure_key);
+            //Restore cart and amount
+            $cart = new Cart($pgzarinpal->cart_id);
+            $customer = new Customer($cart->id_customer);
+            $amount = $cart->getOrderTotal();
 
-        /**
-         * If the order has been validated we try to retrieve it
-         */
-        $order_id = Order::getOrderByCartId((int) $cart->id);
+            //verify the payment
+            $zarinpal = new ZarinPalGateway();
+            $verify = $zarinpal->verifyPayment($amount, $authority);
 
-        if ($order_id && ($secure_key == $customer->secure_key)) {
-            /**
-             * The order has been placed so we redirect the customer on the confirmation page.
-             */
-            $module_id = $this->module->id;
-            Tools::redirect('index.php?controller=order-confirmation&id_cart=' . $cart_id . '&id_module=' . $module_id . '&id_order=' . $order_id . '&key=' . $secure_key);
-        } else {
             /*
-             * An error occured and is shown on a new page.
+             * update order status if verified and there is no payment error
              */
-            $this->errors[] = $this->module->l('An error occured. Please contact the merchant to have more informations');
+            if ( is_numeric($verify) && !$zarinpal->getPaymentError()) {
+                $payment_status = Configuration::get('PS_OS_PAYMENT');
+                $message = "Payment verified success fully. reference ID : {$verify}";
 
-            return $this->setTemplate('error.tpl');
+                /*
+                 * Converting cart into a valid order
+                 */
+                $module_name = $this->module->displayName;
+                $currency_id = $cart->id_currency;
+                $secure_key = $cart->secure_key;
+
+                $this->module->validateOrder($cart->id, $payment_status, $amount, $module_name, $message, array(), $currency_id, false, $secure_key);
+
+                /*
+                 * If the order has been validated we try to retrieve it
+                 */
+                $order_id = Order::getIdByCartId((int)$cart->id);
+
+                if ($order_id && ($secure_key == $customer->secure_key)) {
+                    /*
+                     * The order has been placed so we redirect the customer on the confirmation page.
+                     */
+                    $module_id = $this->module->id;
+
+                    Tools::redirect('index.php?controller=order-confirmation&id_cart=' . $cart->id . '&id_module=' . $module_id . '&id_order=' . $order_id . '&key=' . $secure_key);
+                } else {
+                    /*
+                     * An error occured and is shown on a new page.
+                     */
+                    $error = $this->module->l('An error occured. Please contact the merchant to have more informations');
+                }
+            } else {
+                return $this->setTemplate('module:ps_pgzarinpal/views/templates/front/error.tpl', ['errors' => [$zarinpal->getPaymentError()]]);
+            }
+        } else {
+            return $this->setTemplate('module:ps_pgzarinpal/views/templates/front/error.tpl', ['errors' => $error]);
         }
+        return true;
     }
 }
